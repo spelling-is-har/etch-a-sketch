d35211502b49b1a0e740ad9498cf93cea62a2a36
Jonathan Winter <jonathan.winter@hotmail.com>
2025-11-12 20:27:32 +0000

    Add mouseover functionality to blocks
    Modified createGrid function to add event listener for the blocks that
    make up the grid. The event listener enables mouseover to fill
    in the blocks
    
    	modified:   script.js
    	modified:   styles.css

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ const container = document.querySelector(".container");
 
 function createGrid(gridSize) {
   for (let i = 0; i < gridSize; i++) {
+    //Creates row divs to append the col divs that will create the blocks
     const row = document.createElement("div");
     row.classList.add("row");
+
+    //creates blocks
     for (let j = 0; j < gridSize; j++) {
       const col = document.createElement("div");
       col.classList.add("block");
       col.innerText = "c" + j;
+
+      col.addEventListener("mouseover", (event) => {
+        col.style.background = "red";
+      });
       row.appendChild(col);
     }
 
@@ -16,7 +23,7 @@ function createGrid(gridSize) {
   }
 }
 
-createGrid(3);
+createGrid(16);
 
 //for size
 //create row block
